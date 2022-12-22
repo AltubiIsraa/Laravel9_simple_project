b19344039dfdc9fe02b68c98dc1684f543abe5c2
israaaltubi <israaaltubi@gmail.com>
2022-12-22 14:43:20 +0400

    test

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\Rules;
+
 
 class PasswordController extends Controller
 {
@@ -19,7 +21,10 @@ class PasswordController extends Controller
     {
         $validated = $request->validateWithBag('updatePassword', [
             'current_password' => ['required', 'current_password'],
-            'password' => ['required', Password::defaults(), 'confirmed'],
+            'password' => ['required', 'confirmed', Rules\Password::min(8)
+                ->letters()
+                ->mixedCase()
+                ->numbers()],
         ]);
 
         $request->user()->update([
